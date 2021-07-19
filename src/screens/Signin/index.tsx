04cf3bd1ef8f134deb3166c5ea0f738f6f46b830
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-07-18 23:33:56 -0300

    Aula 01

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,14 +1,39 @@
+
 import React from 'react';
-import { useState } from 'react';
-import { Text, View } from 'react-native';
+import { Image, StatusBar, Text, View } from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png'
+import { ButtonIcon } from '../../components/ButtonIcon';
 import { styles } from './style';
 
 export function Signin() {
-  const [text, setText] = useState("...")
-
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {'\n'}
+          suas jogatinas {'\n'} 
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.8} />
+      </View>
     </View>
   );
 }
