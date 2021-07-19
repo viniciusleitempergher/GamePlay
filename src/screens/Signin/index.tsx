82f925a5f6ced830c0f643127751063d20d4bb3a
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-07-19 11:17:08 -0300

    fontes importadas, texto do signin alterado

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -22,9 +22,9 @@ export function Signin() {
       />
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {'\n'}
-          suas jogatinas {'\n'} 
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'} 
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
